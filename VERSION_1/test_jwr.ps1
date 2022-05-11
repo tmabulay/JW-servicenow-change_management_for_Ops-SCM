8e96ab2560c305622848bc2d78b5541e7a97338b
tmabulay <105349998+tmabulay@users.noreply.github.com>
2022-05-11 14:25:45 +0300

    jwr command skeletton

--- VERSION_1/test_jwr.ps1
+++ VERSION_1/test_jwr.ps1
@@ -1,1 +1,21 @@
-﻿
+﻿# jwr create-release 2021-11-25 -complexity easy -verification moderate
+# https://www.improvescripting.com/how-to-create-custom-powershell-cmdlet-step-by-step/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# PRINT ON SCREEN
+    # Release RLSE0019760 and change CHG0140269 for "NCTool 2021-11-25" create in ServiceNow.
+    # Risk assessed to "Moderate"
+

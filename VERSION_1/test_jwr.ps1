73907e0e87dc8df33279115973ede64753a281a3
tmabulay <105349998+tmabulay@users.noreply.github.com>
2022-05-12 01:02:00 +0300

    jwr script skeleton

--- VERSION_1/test_jwr.ps1
+++ VERSION_1/test_jwr.ps1
@@ -2,6 +2,31 @@
 # https://www.improvescripting.com/how-to-create-custom-powershell-cmdlet-step-by-step/
 
 
+<# 1. CREATE CHANGE REQUEST
+    INPUT DEFAULT
+
+    INPUT TO ADD
+        business_service
+        caused_by_another_change: NO
+        short_description
+        description
+        category
+#> 
+$business_service           = Read-Host -Prompt 'Business Service '
+$caused_by_another_change   = Read-Host -Prompt 'Caused by another change(Y/N) '
+$short_description          = Read-Host -Prompt 'Short Description '
+$description                = Read-Host -Prompt 'Description '
+
+<# 1. CREATE A RELEASE
+    INPUT
+        business_service
+        priority
+        change_request
+        state
+        assignment_group
+        assigned_to
+#> 
+
 
 
 
